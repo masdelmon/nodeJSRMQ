f063392dbd6a6a88fe3edf0c97f4736f4f845921
Massimiliano <masdelmon@hotmail.it>
2019-01-21 18:12:19 +0100

    Update connection.js

--- app/connection/connection.js
+++ app/connection/connection.js
@@ -1,7 +1,7 @@
 var amqp = require('amqplib/callback_api')
 
 module.exports = (callback) => {
-  amqp.connect('amqp://guest:guest@192.168.0.29:15672',
+  amqp.connect('amqp://guest:guest@192.168.0.29',
     (error, conection) => {
     if (error) {
       throw new Error(error);
