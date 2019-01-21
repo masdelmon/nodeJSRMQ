8d1dd002c49044d551b96c7969a09a2c23e587c2
Massimiliano <masdelmon@hotmail.it>
2019-01-21 17:05:31 +0100

    Update connection.js

--- app/connection/connection.js
+++ app/connection/connection.js
@@ -1,7 +1,7 @@
 var amqp = require('amqplib/callback_api')
 
 module.exports = (callback) => {
-  amqp.connect('amqp://guest:guest@ip172-18-0-12-bh2u278v0j3g00aqh9j0-8080.direct.labs.play-with-docker.com:8080',
+  amqp.connect('amqp://guest:guest@ip172-18-0-12-bh2u278v0j3g00aqh9j0-8080.direct.labs.play-with-docker.com:5672',
     (error, conection) => {
     if (error) {
       throw new Error(error);
