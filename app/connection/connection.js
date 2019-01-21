2616fa35ac39ec8fa45fdd38d3781506e1233242
Massimiliano <masdelmon@hotmail.it>
2019-01-21 23:14:29 +0100

    Update connection.js

--- app/connection/connection.js
+++ app/connection/connection.js
@@ -1,7 +1,7 @@
 var amqp = require('amqplib/callback_api')
 
 module.exports = (callback) => {
-  amqp.connect(process.env.CLOUDAMQP_URL + "?heartbeat=60"  ,
+  amqp.connect('amqp://' + process.env.CLOUDAMQP_URL + "?heartbeat=60"  ,
     (error, conection) => {
     if (error) {
       throw new Error(error);
